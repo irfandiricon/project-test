42591b86469532e15f7adf218bb736a1684899cc
Irfandi Ricon <irfandi.ricon@anteraja.id>
2025-11-20 10:42:34 +0700

    List User

--- userController.go
+++ userController.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"project-test/models"
+)
+
+type UserService struct {
+	data map[string]*models.User
+}
+
+func NewUserService() *UserService {
+	return &UserService{
+		data: make(map[string]*models.User),
+	}
+}
+
+func (s *UserService) GetAll() ([]*models.User, error) {
+	result := []*models.User{}
+	for _, v := range s.data {
+		result = append(result, v)
+	}
+	return result, nil
+}
